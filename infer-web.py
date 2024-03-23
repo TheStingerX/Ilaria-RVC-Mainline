b793ea16d3f9fc18687ec46df60a993bed313d82
Ilaria <108286953+TheStingerX@users.noreply.github.com>
2024-03-23 18:49:20 +0100

    Update infer-web.py

--- infer-web.py
+++ infer-web.py
@@ -10,6 +10,9 @@ from infer.modules.vc.modules import VC
 from i18n.i18n import I18nAuto
 from configs.config import Config
 from sklearn.cluster import MiniBatchKMeans
+import matplotlib.pyplot as plt
+import soundfile as sf
+import requests
 import torch
 import numpy as np
 import gradio as gr
@@ -213,6 +216,19 @@ sr_dict = {
     "48k": 48000,
 }
 
+def download_file(url):
+    # Estrai l'ID del file dal link di Google Drive
+    file_id = url.split('/')[-2]
+
+    # Crea il link di download diretto
+    download_url = f'https://docs.google.com/uc?export=download&id={file_id}'
+
+    # Scarica il file
+    response = requests.get(download_url, allow_redirects=True)
+    local_filename = url.split('/')[-1] + '.wav'
+    open(local_filename, 'wb').write(response.content)
+
+    return local_filename
 
 def if_done(done, p):
     while 1:
@@ -487,6 +503,57 @@ def get_pretrained_models(path_str, f0_str, sr2):
         else "",
     )
 
+def create_spectrogram_and_get_info(audio_file):
+    # Clear figure in case it has data in it
+    plt.clf()
+    
+    # Read the audio data from the file
+    audio_data, sample_rate = sf.read(audio_file)
+
+    # Convert to mono if it's not mono
+    if len(audio_data.shape) > 1:
+        audio_data = np.mean(audio_data, axis=1)
+
+    # Create the spectrogram
+    plt.specgram(audio_data, Fs=sample_rate / 1, NFFT=4096, sides='onesided',
+                 cmap="Reds_r", scale_by_freq=True, scale='dB', mode='magnitude', window=np.hanning(4096))
+
+    # Save the spectrogram to a PNG file
+    plt.savefig('spectrogram.png')
+
+    # Get the audio file info
+    audio_info = sf.info(audio_file)
+    
+    bit_depth = {'PCM_16': 16, 'FLOAT': 32}.get(audio_info.subtype, 0)
+    
+    # Convert duration to minutes, seconds, and milliseconds
+    minutes, seconds = divmod(audio_info.duration, 60)
+    seconds, milliseconds = divmod(seconds, 1)
+    milliseconds *= 1000  # convert from seconds to milliseconds
+    
+    # Convert bitrate to mb/s
+    bitrate = audio_info.samplerate * audio_info.channels * bit_depth / 8 / 1024 / 1024
+    
+    # Calculate speed in kbps
+    speed_in_kbps = audio_info.samplerate * bit_depth / 1000
+    
+    # Create a table with the audio file info
+    filename_without_extension, _ = os.path.splitext(os.path.basename(audio_file))
+    info_table = f"""
+    
+    | Information | Value |
+    | :---: | :---: |
+    | File Name | {filename_without_extension} |
+    | Duration | {int(minutes)} minutes - {int(seconds)} seconds - {int(milliseconds)} milliseconds |
+    | Bitrate | {speed_in_kbps} kbp/s |
+    | Audio Channels | {audio_info.channels} |
+    | Samples per second | {audio_info.samplerate} Hz |
+    | Bit per second | {audio_info.samplerate * audio_info.channels * bit_depth} bit/s |
+    
+    """
+    
+    # Return the PNG file of the spectrogram and the info table
+    return info_table, 'spectrogram.png'
 
 def change_sr2(sr2, if_f0_3, version19):
     path_str = "" if version19 == "v1" else "_v2"
@@ -1284,25 +1351,46 @@ with gr.Blocks(title="Ilaria RVC 💖") as app:
                     but4.click(train_index, [exp_dir1, version19], info3)
         
         with gr.TabItem(i18n("Extra")):
-            with gr.Accordion('Credits', open=False):
-                gr.Markdown('''
-                ## All the amazing people who worked on this!
-                - **Ilaria**: Founder, Lead Developer
-                - **Yui**: Training feature
-                - **GDR-**: Inference feature
-                - **Poopmaster**: Model downloader, Model importer
-                - **kitlemonfoot**: Ilaria TTS implementation
-                - **eddycrack864**: UVR5 implementation
-                                
-                - **Charlotte**: Beta Tester
-                - **RME**: Beta Tester
-                - **Delik**: Beta Tester
-                                
-                - **yumereborn**: Ilaira RVC image
-                                
-                ### **In loving memory of JLabDX** 🕊️
-                ''')
-                              
+        
+            with gr.Row():
+                with gr.Accordion('Ilaria Audio Analyzer', open=False):
+                    with gr.Column():
+                        audio_input = gr.Audio(type='filepath')
+                        create_spec_butt = gr.Button(value='Create Spectrogram And Get Info', variant='primary')
+
+                        with gr.Column():
+                            output_markdown = gr.Markdown(value="", visible=True)
+                            image_output = gr.Image(type='filepath', interactive=False)
+                    
+                            with gr.Accordion('Audio Downloader', open=False):
+                                url_input = gr.Textbox(value='', label='Google Drive Audio URL')
+                                download_butt = gr.Button(value='Download audio', variant='primary')
+                            
+                            download_butt.click(fn=download_file, inputs=[url_input], outputs=[audio_input])
+                            create_spec_butt.click(fn=create_spectrogram_and_get_info, inputs=[audio_input], outputs=[output_markdown, image_output])
+                        
+                            download_butt.click(fn=download_file, inputs=[url_input], outputs=[audio_input])
+                            create_spec_butt.click(fn=create_spectrogram_and_get_info, inputs=[audio_input], outputs=[output_markdown, image_output])
+                            
+                with gr.Accordion('Credits', open=False):
+                    gr.Markdown('''
+                    ## All the amazing people who worked on this!
+                    - **Ilaria**: Founder, Lead Developer
+                    - **Yui**: Training feature
+                    - **GDR-**: Inference feature
+                    - **Poopmaster**: Model downloader, Model importer
+                    - **kitlemonfoot**: Ilaria TTS implementation
+                    - **eddycrack864**: UVR5 implementation
+                                    
+                    - **Charlotte**: Beta Tester
+                    - **RME**: Beta Tester
+                    - **Delik**: Beta Tester
+                                    
+                    - **yumereborn**: Ilaira RVC image
+                                    
+                    ### **In loving memory of JLabDX** 🕊️
+                    ''')
+                                           
         with gr.TabItem(i18n("")):
             gr.Markdown('''
                 ![ilaria](https://i.ytimg.com/vi/5PWqt2Wg-us/maxresdefault.jpg)
