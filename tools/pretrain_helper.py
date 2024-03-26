a25da3da30f77a67f809463946d02596f65511df
miqu-s <160888205+miqu-s@users.noreply.github.com>
2024-03-26 01:37:08 +0000

    Update pretrain_helper.py

--- tools/pretrain_helper.py
+++ tools/pretrain_helper.py
@@ -43,7 +43,7 @@ def download_file(url, filename, current_file, total_files):
     try:
         response = requests.get(url, stream=True)
         if response.status_code == 200:
-            directory = "assets/pretrains_v2"
+            directory = "assets/pretrained_v2"
             if not os.path.exists(directory):
                 os.makedirs(directory)
             
