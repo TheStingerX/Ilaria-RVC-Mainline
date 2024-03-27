5d4e5a362cadc0736f8c2af04c90fcc51adea8c2
miqu-s <160888205+miqu-s@users.noreply.github.com>
2024-03-27 22:18:17 +0000

    Add files via upload

--- infer/lib/audio.py
+++ infer/lib/audio.py
@@ -31,8 +31,8 @@ def wav2(i, o, format):
 
 
 def audio2(i, o, format, sr):
-    inp = av.open(i, "rb")
-    out = av.open(o, "wb", format=format)
+    inp = av.open(i, "r")
+    out = av.open(o, "w", format=format)
     if format == "ogg":
         format = "libvorbis"
     if format == "f32le":
