6e40d60bdd190433778ef730717e6bafdc90c6ff
Mitsubachi1 <60766345+Mitsubachi1@users.noreply.github.com>
2024-03-31 10:35:59 -0700

    Workaround for audio-separator

--- Ilaria-RVC-Launcher.bat
+++ Ilaria-RVC-Launcher.bat
@@ -1,3 +1,6 @@
 @echo off
+echo Installing audio-separator workaround
+runtime\python.exe -m pip install audio-separator
+runtime\python.exe -m pip install audio-separator[gpu]
 echo Ilaria RVC is starting...
 start /B runtime\python.exe infer-web.py --pycmd runtime\python.exe --port 7897
